7d9b1af7f2c71af6ec1b80651b199ae3ce8dbd74
Xavier <xaviersiao.23@ichat.sp.edu.sg>
2024-04-25 15:23:03 +0800

    Updated

--- bmi.py
+++ bmi.py
@@ -1,5 +1,26 @@
 def calculate_bmi(height,weight):
     print("Height="+ str(height))
+    
     print("Weight="+ str(weight))
 
-calculate_bmi(weight=57,height = 1.73)
+    bmi = weight/(height ** 2)
+    bmi = round(bmi,2)
+    if bmi<18.5:
+        print("You are under weight")
+    elif bmi>=18.5 and bmi<=25.0:
+        print("You are normal weight")
+    else:
+        print("You are overweight")
+
+
+
+    return str(bmi)
+
+
+
+
+
+
+
+
+print("BMI:"+calculate_bmi(1.73,57))
